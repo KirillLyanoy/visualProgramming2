296a27e3472a2231521daeb9038a7bf84d30e884
lyanoy <uwaltonsimons@gmail.com>
2025-02-10 16:56:39 +0700

    edit lab1

--- FiguresArea/main.cpp
+++ FiguresArea/main.cpp
@@ -1,10 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QVBoxLayout>
+#include <QLabel>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QWidget *centralWidget = new QWidget(&w);
+
+    QVBoxLayout *Layout1 = new QVBoxLayout;
+
+    QLabel *label1 = new QLabel("Выберите фигуру:");
+    QLabel *label2 = new QLabel("Выберите формулу:");
+
+    Layout1->addWidget(label1);
+    Layout1->addWidget(label2);
+
+    centralWidget->setLayout(Layout1);
+
+    w.setCentralWidget(centralWidget);
     w.show();
 
     return a.exec();
